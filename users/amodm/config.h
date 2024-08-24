ab575b2e59e93c449a3269152656d5375ec904cc
Amod Mulay <amodmulay1@gmail.com>
2024-08-23 19:15:32 -0700

    add basic configs based on holykeebs

---
+++ users/amodm/config.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#undef MASTER_LEFT
+#undef EE_HANDS
+#undef MASTER_RIGHT
+
+#define SERIAL_USART_TX_PIN GP1
+
+// pointing device on right, default to right.
+#define MASTER_RIGHT
+
+// Generic Cirque configuration.
+#ifdef POINTING_DEVICE_DRIVER_cirque_pinnacle_i2c
+    #if defined(POINTING_DEVICE_CONFIGURATION_CIRQUE35) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE35_CIRQUE35) || defined(POINTING_DEVICE_CONFIGURATION_PIMORONI_CIRQUE35) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE35_PIMORONI) || defined(POINTING_DEVICE_CONFIGURATION_TRACKPOINT_CIRQUE35) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE35_TRACKPOINT)
+        #define CIRQUE_PINNACLE_DIAMETER_MM 35
+    #elif defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40_CIRQUE40) || defined(POINTING_DEVICE_CONFIGURATION_PIMORONI_CIRQUE40) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40_PIMORONI) || defined(POINTING_DEVICE_CONFIGURATION_TRACKPOINT_CIRQUE40) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40_TRACKPOINT)
+        #define CIRQUE_PINNACLE_DIAMETER_MM 40
+    #else
+        #error "Unknown Cirque configuration."
+    #endif
+    // Cirque 40mm on the left side of a split keyboard is rotated 180 degrees.
+    #if defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40) && defined(POINTING_DEVICE_POSITION_LEFT)
+        #define POINTING_DEVICE_ROTATION_180
+    #endif
+    #if defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40_PIMORONI) || defined(POINTING_DEVICE_CONFIGURATION_CIRQUE40_TRACKPOINT)
+        #define POINTING_DEVICE_ROTATION_180
+    #endif
+    // Tap for left click.
+    #define CIRQUE_PINNACLE_TAP_ENABLE
+    #define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+#endif
+
+#ifdef POINTING_DEVICE_DRIVER_ps2
+    // The default streaming mode is flaky and doesn't always work. This setting doesn't seem to affect performance.
+    #define PS2_MOUSE_USE_REMOTE_MODE
+
+    // Serial uses PIO0, change PS2 to PIO1.
+    #define PS2_PIO_USE_PIO1
+
+    #define PS2_MOUSE_INVERT_X
+    #define PS2_MOUSE_INVERT_Y
+
+    #define PS2_DATA_PIN    GP2
+    #define PS2_CLOCK_PIN   GP3
+
+    // Start faster when keyboard resets.
+    #define PS2_MOUSE_INIT_DELAY 500
+#endif
+
+#ifdef OLED_ENABLE
+    #define SPLIT_LAYER_STATE_ENABLE
+#endif
