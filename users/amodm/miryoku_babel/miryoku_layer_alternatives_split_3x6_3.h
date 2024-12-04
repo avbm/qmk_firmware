a78d2f0582beb4ed94d66990e620b1978fdd55e3
Amod Mulay <amodmulay1@gmail.com>
2024-12-04 14:31:19 -0800

    remove backspace from pinky

--- users/amodm/miryoku_babel/miryoku_layer_alternatives_split_3x6_3.h
+++ users/amodm/miryoku_babel/miryoku_layer_alternatives_split_3x6_3.h
@@ -12,13 +12,13 @@
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY_3x6_3 \
 KC_TAB,           KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,                 KC_MINS,\
 KC_ESC,           LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              RSFT_T(KC_J),      RCTL_T(KC_K),      RALT_T(KC_L),      RGUI_T(KC_QUOT),      KC_COLN,\
-KC_LSFT,          LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH), KC_BSPC,\
+KC_LSFT,          LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH), KC_RSFT,\
 U_NP,             U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),KC_ENT,            MO(U_NUM),         LT(U_FUN,KC_DEL),  U_NP,              U_NP,                 U_NP
 
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTY_3x6_3 \
 TD(U_TD_U_BASE),  KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              KC_MINS,\
 KC_ESC,           KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,           KC_COLN,\
-KC_LSFT,          KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           KC_BSPC,\
+KC_LSFT,          KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           KC_RSFT,\
 U_NP,             U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_NAV_3x6_3 \
